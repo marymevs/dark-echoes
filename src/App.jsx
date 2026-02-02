7d9fe9f2616d76bd79442004eaa7e71fa08b2b27
Mary Imevbore <mimevbore14@gmail.com>
2026-02-02 18:46:53 -0500

    render page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,57 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>Episode {selectedEpisode.id}</h2>
+        <h3>{selectedEpisode.title}</h3>
+        <p>{selectedEpisode.description}</p>
+        <button
+          onClick={() => {
+            console.log(`watching ${selectedEpisode.title}`);
+          }}
+        >
+          Watch now!
+        </button>
+      </section>
+    );
+  }
+
+  function Episodes() {
+    return (
+      <ul>
+        {episodes.map((episode) => (
+          <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+            {episode.title}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <Episodes />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
